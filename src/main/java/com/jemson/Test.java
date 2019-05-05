ae4bd5daf4ae15daed082c8abbdc2d33b50c041e
JemsonSu <suzhixian@dingtalk.com>
2019-05-05 16:58:32 +0800

    提加pom.xml  Test.java 文件

---
+++ src/main/java/com/jemson/Test.java
@@ -0,0 +1,14 @@
+package com.jemson;
+/**
+ * 项目主要测试类，测试完毕即删除测试内容
+ * @author xian
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to test !");
+
+	}
+
+}
