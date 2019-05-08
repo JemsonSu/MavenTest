a1871750585d6d07ddfae50ba0d657dd1d75837c
JemsonSu123456 <suzhixian@dingtalk.com>
2019-05-08 09:02:43 +0800

    添加新文件

---
+++ src/main/java/com/jemson/spark/KuduSpark.java
@@ -0,0 +1,14 @@
+package com.jemson.spark;
+/**
+   * 用spark去处理创建然后进入kudu库
+ * @author xian
+ *
+ */
+public class KuduSpark {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/jemson/test/MyTest.java
@@ -0,0 +1,17 @@
+package com.jemson.test;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		System.out.println("this is a test!");
+
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println(1+1);
+	}
+
+}
