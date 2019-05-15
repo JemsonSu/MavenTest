a93f56015d22b6905168835d756e1c1ad0f47744
JemsonSu123456 <suzhixian@dingtalk.com>
2019-05-15 11:00:38 +0800

    测试jdk8 lambda  * 内置四大核心函数式接口

---
+++ src/test/java/com/jemson/test/testjdk8/TestLambda3.java
@@ -0,0 +1,36 @@
+package com.jemson.test.testjdk8;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+/**
+ * 测试jdk8 lambda
+ * 内置四大核心函数式接口
+ * 1.Consumer<T> : 消费型接口
+		void accept(T t);
+		
+	2.Supplier<T> : 供给型接口
+		T get();
+		
+	3.Function<T, R> : 函数型接口
+		R apply(T t);
+		
+	4.Predicate<T> : 断言型接口
+		boolean test(T t);
+		
+ * @author xian
+ *
+ */
+public class TestLambda3 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	
+
+}
